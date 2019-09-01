5fa13451b956db4ab3ec86ea82d6b66173057777
Perezonance <53836343+Perezonance@users.noreply.github.com>
2019-09-01 14:40:18 -0400

    Update DBUtility.java

--- src/dbUtilities/DBUtility.java
+++ src/dbUtilities/DBUtility.java
@@ -21,20 +21,21 @@ import java.util.Set;
 public class DBUtility {
 	
 	private Properties prop = new Properties();
+	private static Connection connection;
 	
 	File propFile;
 	
 	public DBUtility() {
 		this(new File("db_default.properties"));
-		
 	}
-	
 	public DBUtility(File f) {
 		try {
 			if(f.exists()) {
 				propFile = f;
 				InputStream input = new FileInputStream(f);
 				prop.load(input);
+				
+				connection = DBConnection.connect(f);
 			}else {
 				
 				OutputStream output = new FileOutputStream(f);
@@ -56,14 +57,21 @@ public class DBUtility {
 			e.printStackTrace();
 		}
 	}
-	
+	public static Connection getDBInstance() {
+		return connection;
+	}
 	public static String testConnection(Connection con){
 		if(con == null){
 			return "Not Connected";
 		}else
 			return "Connected";
 	}
-	
+	public static String testConnection(){
+		if(connection == null){
+			return "Not Connected";
+		}else
+			return "Connected";
+	}
 	public static String closeConnection(Connection connection){
 		if(connection == null){
 			return "No connection available to close!";
@@ -81,7 +89,23 @@ public class DBUtility {
 			}
 		}
 	}
-	
+	public static String closeConnection(){
+		if(connection == null){
+			return "No connection available to close!";
+		}else{
+			try {
+				connection.close();
+				return "Connection to DB closed";
+			}catch(SQLException e){
+				e.printStackTrace();
+				return "Connection Failed";
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				return "Connection Failed";
+			}
+		}
+	}
 	public static String saveData(String tableName, HashMap<String, String> hm, Connection con){
 		String query = "INSERT INTO " + tableName + "( ";
 		String values = "(";
@@ -120,9 +144,45 @@ public class DBUtility {
 		}
 		return (x + " Rows affected!");
 	}
-	
-	public static boolean insertData(String query, Connection con) {
+	public static String saveData(String tableName, HashMap<String, String> hm){
+		String query = "INSERT INTO " + tableName + "( ";
+		String values = "(";
+		int x = 0;
 		
+		Set<String> keys = hm.keySet();
+		Iterator iter = keys.iterator();
+		int counter = 0;
+		while(iter.hasNext()) {
+			if(counter > 0) {
+				Object keyLast = iter.next();
+				query += ", " + keyLast;
+				values += "', '"  + hm.get(keyLast);
+			}else {
+				Object keyLast = iter.next();
+				counter++;
+				query += keyLast;
+				values += "'" + hm.get(keyLast);
+			}	
+		}
+		query += ") VALUES ";
+		values += "');";
+		query += values;
+		System.out.println(query);
+		
+		
+		Statement st;
+		try {
+			st = connection.createStatement();
+			x = st.executeUpdate(query);
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return (x + " Rows affected!");
+	}
+	public static boolean updateQuery(String query, Connection con) {
 		Statement st;
 		try {
 			
@@ -138,12 +198,42 @@ public class DBUtility {
 		}
 		return true;
 	}
+	public static int updateQuery(String query) {
+		Statement st;
+		int rowsAffected = 0;
+		try {
+			
+			 st = connection.createStatement();
+			 rowsAffected = st.executeUpdate(query);    
+		} catch(SQLException e) {
+			e.printStackTrace();
+			rowsAffected = -1;
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			rowsAffected = -1;
+		}
+		return rowsAffected;
+	}
 	public static ResultSet executeQuery(String query, Connection con) {
 		ResultSet rs = null;
 		Statement st;
 		try {
-			
 			 st = con.createStatement();
+			 rs = st.executeQuery(query);
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return rs;
+	}
+	public static ResultSet executeQuery(String query) {
+		ResultSet rs = null;
+		Statement st;
+		try {
+			 st = connection.createStatement();
 			 rs = st.executeQuery(query);
 		} catch(SQLException e) {
 			e.printStackTrace();
@@ -172,6 +262,4 @@ public class DBUtility {
 		}
 		return "";
 	}
-
-	
 }
