50e2368be884f91a5bb95fe2d603aa8f0c4a8041
Perezonance <53836343+Perezonance@users.noreply.github.com>
2019-09-03 10:45:41 -0400

    Update to User Dashboard Functionality

--- WebContent/UserDashboard.jsp
+++ WebContent/UserDashboard.jsp
@@ -24,66 +24,16 @@
 		ResultSet rs = DBUtility.executeQuery(query);
 		rs.next();
 		String fName = rs.getString("user_first_name");
-		
 			%>
 		<h1>Welcome to Quiz Lit, <%= fName %>!</h1>
 		<br>
 		<div class="category-table">
-			<table class="fixed_header">
-			  <thead>
-			    <tr>
-			      <th>Quiz Category</th>
-			      <th>Description</th>
-			      <th>Average Score</th>
-			      <th>Take Quiz</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			  	
-			    <tr>
-			      <td>row 1-0</td>
-			      <td>row 1-1</td>
-			      <td>row 1-2</td>
-			      <td>row 1-3</td>
-			    </tr>
-			    <tr>
-			      <td>row 2-0</td>
-			      <td>row 2-1</td>
-			      <td>row 2-2</td>
-			      <td>row 2-3</td>
-			    </tr>
-			    <tr>
-			      <td>row 3-0</td>
-			      <td>row 3-1</td>
-			      <td>row 3-2</td>
-			      <td>row 3-3</td>
-			    </tr>
-			    <tr>
-			      <td>row 4-0</td>
-			      <td>row 4-1</td>
-			      <td>row 4-2</td>
-			      <td>row 4-3</td>
-			    </tr>
-			    <tr>
-			      <td>row 5-0</td>
-			      <td>row 5-1</td>
-			      <td>row 5-2</td>
-			      <td>row 5-3</td>
-			    </tr>
-			    <tr>
-			      <td>row 6-0</td>
-			      <td>row 6-1</td>
-			      <td>row 6-2</td>
-			      <td>row 6-3</td>
-			    </tr>
-			    <tr>
-			      <td>row 7-0</td>
-			      <td>row 7-1</td>
-			      <td>row 7-2</td>
-			      <td>row 7-3</td>
-			    </tr>
-			  </tbody>
-			</table>
+			
+			  	<% RequestDispatcher rd = request.getRequestDispatcher("CategoryTableController");
+			  	rd.include(request, response); %>
+			   
+			  
+			
 		</div>
 	</body>
 </html>
--- src/controllers/CategoryTableController.java
+++ src/controllers/CategoryTableController.java
@@ -1,12 +1,18 @@
 package controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dbUtilities.DBUtility;
 
 /**
  * Servlet implementation class CategoryTableController
@@ -42,7 +48,7 @@ public class CategoryTableController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	/**
@@ -50,7 +56,46 @@ public class CategoryTableController extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		//All Output will be translated as html text
+		response.setContentType("text/html");
+		
+		String queryCat = "SELECT * FROM `quizit`.`category`;";
+		
+		/*
+		 * <form action=\"QuizLandingPage.jsp\" method=\"post\">"
+							+ "<input type=\"submit\" name=\"" + rsCat.getString("category_name") + "\">Take Quiz!</input>"
+						+ "</form>
+		 */
+		
+		ResultSet rsCat = DBUtility.executeQuery(queryCat);
+		PrintWriter out = response.getWriter();
+		out.println("<table class=\"fixed_header\">");
+		out.println("<thead>");
+		out.println("<tr>");
+		out.println("<th>Quiz Category</th>");
+		out.println("<th>Description</th>");
+		out.println("<th>Take Quiz</th>");
+		out.println("</thead>");
+		out.println("</tr>");
+		try {
+			while(rsCat.next()) {
+				out.println("<tr>");
+				out.println("<td>" + rsCat.getString("category_name") + "</td>");
+				out.println("<td>" + rsCat.getString("category_description") + "</td>");
+				out.println("<td><a href=\"QuizLandingPage.jsp\"><button>Take Quiz</button></a></td>");
+				out.println("</tr>");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		out.println("</tbody>");
+		out.println("</table>");
+		out.flush();
+		
 	}
 
 }
